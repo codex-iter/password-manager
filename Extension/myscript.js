00088e5cab58bdd544f73233a4e0a203c9a103b2
Pawan kumar <32352500+Pawan0411@users.noreply.github.com>
2018-03-17 22:39:27 +0530

    Update myscript.js

--- Extension/myscript.js
+++ Extension/myscript.js
@@ -1,18 +1,6 @@
-/*var console = chrome.extension.getBackgroundPage().console;
-var email = document.getElementById(document.getElementsByTagName('title')[0].innerHTML);
-var app = {
-    init: function() {
-       
-        var cb = document.getElementById('save');
-
-        cb.addEventListener('click', function() {
-            chrome.runtime.sendMessage(email.value);
-        });
-    }
-};
-document.addEventListener("DOMContentLoaded", function() {
-    app.init();
-});*/
-
-chrome.runtime.sendMessage(document.getElementsByTagName('title')[0].innerHTML);
-//alert("Data-Saved");
+/*var pass = chrome.runtime.sendMessage(document.getElementById("pass").value = "12456789" &&
+    document.getElementById("email").value); //for facebook*/
+chrome.runtime.sendMessage(document.getElementById("email").value);
+//var email = ; //for facebook
+//document.getElementById("session_password-login").value;//for linkedin
+//document.getElementById("login-password").value;// for linkedin
