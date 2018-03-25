501fe56bea1a530f32b0d3e239bec812d14b10ca
Pawan kumar <32352500+Pawan0411@users.noreply.github.com>
2018-03-25 20:53:15 +0530

    Update background.js

--- Extension/background.js
+++ Extension/background.js
@@ -14,7 +14,7 @@ background.init();*/
 chrome.runtime.onMessage.addListener(function(request,sender,sendResponse){
 console.log(request);
 var store=request;
-localStorage['Title']=store;
+localStorage['username']=store;
 
 //localStorage.setItem("Title",request);
 });
