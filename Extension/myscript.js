4d57e828aec36ef00dce51daa0e6cbace217e4f7
aruba246 <arubas113@gmail.com>
2018-03-25 17:19:22 +0530

    Removed myscript.js

--- Extension/myscript.js
+++
@@ -1,6 +0,0 @@
-/*var pass = chrome.runtime.sendMessage(document.getElementById("pass").value = "12456789" &&
-    document.getElementById("email").value); //for facebook*/
-chrome.runtime.sendMessage(document.getElementById("email").value);
-//var email = ; //for facebook
-//document.getElementById("session_password-login").value;//for linkedin
-//document.getElementById("login-password").value;// for linkedin
