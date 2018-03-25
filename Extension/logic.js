53ed2c4f93896f1ff3198c458bbe49bf12c517ca
aruba246 <arubas113@gmail.com>
2018-03-25 17:17:45 +0530

    Removed logic.js

--- Extension/logic.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded',function(){
-    var link=document.getElementById('link');
-    //onClick's Logic Below:
-    link.addEventListener('click',function() {
-        link.innerHTML="You Clicked Me";
-    });
-});
