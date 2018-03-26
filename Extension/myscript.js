36a87a76c56767c2f9521f3d84e6f141ab8f1957
Pawan kumar <32352500+Pawan0411@users.noreply.github.com>
2018-03-26 21:20:53 +0530

    Update myscript.js

--- Extension/myscript.js
+++ Extension/myscript.js
@@ -1,6 +1,7 @@
 /*var pass = chrome.runtime.sendMessage(document.getElementById("pass").value = "12456789" &&
     document.getElementById("email").value); //for facebook*/
-chrome.runtime.sendMessage(document.getElementById("email").value);
+document.getElementById('email').value = email; //wants to grab here
+document.getElementById('pass').value = pass;
 //var email = ; //for facebook
 //document.getElementById("session_password-login").value;//for linkedin
 //document.getElementById("login-password").value;// for linkedin
