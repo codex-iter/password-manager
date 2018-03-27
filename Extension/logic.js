68a88eac2fe835542686332910a103868f34d4fe
Pawan kumar <32352500+Pawan0411@users.noreply.github.com>
2018-03-27 22:32:33 +0530

    Update logic.js

--- Extension/logic.js
+++ Extension/logic.js
@@ -1,9 +1,22 @@
-var email; //declared global
-var pass;
+function popup() {
+    chrome.tabs.query({ currentWindow: true, active: true }, function(tabs) {
+        var activeTab = tabs[0];
+        let msg = {
+            message: "start",
+            email: document.getElementById('user').value,
+            pass: document.getElementById('pswd').value
+        }
+        chrome.tabs.sendMessage(activeTab.id, msg);
+    });
+}
+
 document.addEventListener('DOMContentLoaded', function() {
     var checkPageButton = document.getElementById('save');
     checkPageButton.addEventListener('click', function() {
-        email = chrome.runtime.sendMessage(document.getElementById('user').value);
-        pass = chrome.runtime.sendMessage(document.getElementById('pswd').value);
+        var email = chrome.runtime.sendMessage(document.getElementById('user').value);
+        var pass = chrome.runtime.sendMessage(document.getElementById('pswd').value);
     }, false);
 }, false);
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("save").addEventListener("click", popup);
+});
