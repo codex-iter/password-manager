1d3289e5b05eb663b3e279274b79539b858e2f23
Pawan kumar <32352500+Pawan0411@users.noreply.github.com>
2018-03-28 19:14:46 +0530

    Update background.js

--- Extension/background.js
+++ Extension/background.js
@@ -1,20 +1,6 @@
-/*var background = {
-    init: function() {
-        var Username = [];
-        chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-            console.log("Username : " + request);
-            var store = request;
-            localStorage.setItem("Username", store);
-            
 
-        });
-    }
-};
-background.init();*/
-chrome.runtime.onMessage.addListener(function(request,sender,sendResponse){
-console.log(request);
-var store=request;
-localStorage['username']=store;
-
-//localStorage.setItem("Title",request);
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    console.log("username : " + request.email);
+    console.log("password : " + request.pass);
+    localStorage.setItem(request.email, request.pass);
 });
