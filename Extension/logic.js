41793be3d5bd9c2a36f262fcc606db1510d4db7d
Pawan kumar <32352500+Pawan0411@users.noreply.github.com>
2018-03-29 22:53:34 +0530

    Update logic.js

--- Extension/logic.js
+++ Extension/logic.js
@@ -1,3 +1,5 @@
+var CryptoJS = require('crypto-js');
+
 function popup() {
     chrome.tabs.query({ currentWindow: true, active: true }, function(tabs) {
         var activeTab = tabs[0];
@@ -12,12 +14,17 @@ function popup() {
 document.addEventListener('DOMContentLoaded', function() {
     var checkPageButton = document.getElementById('save');
     checkPageButton.addEventListener('click', function() {
-        var email = document.getElementById('user').value;
-        var pass = document.getElementById('pswd').value;
-         var hash = CryptoJS.SHA3(pass);
-         localStorage.setItem(email, hash);
-    }, false);
+            var email = document.getElementById('user').value;
+            var pass = document.getElementById('pswd').value;
+            var hash = CryptoJS.SHA3(pass);
+            localStorage.setItem(email, hash);
+        },
+        false);
 }, false);
 document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("save").addEventListener("click", popup);
 });
+/** For example !! */
+var abc = "sdsabfkjdbvkdbkvgwd";
+var hash = CryptoJS.SHA3(abc);
+alert(hash);
