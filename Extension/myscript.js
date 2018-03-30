c06cdc9f19b3eed65ec6dd00deef8085e03a35e8
Pawan kumar <32352500+Pawan0411@users.noreply.github.com>
2018-03-30 20:29:29 +0530

    Update myscript.js

--- Extension/myscript.js
+++ Extension/myscript.js
@@ -1,7 +1,9 @@
-/*var pass = chrome.runtime.sendMessage(document.getElementById("pass").value = "12456789" &&
-    document.getElementById("email").value); //for facebook*/
-document.getElementById('email').value = email; //wants to grab here
-document.getElementById('pass').value = pass;
-//var email = ; //for facebook
-//document.getElementById("session_password-login").value;//for linkedin
-//document.getElementById("login-password").value;// for linkedin
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        if (request.message === "start") {
+            document.getElementById('email').value = request.email;
+            document.getElementById('pass').value = request.pass;
+
+        }
+    }
+);
