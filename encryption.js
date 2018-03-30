436d639c68d9fd3c1453340de8527ecbd38bba7e
Aruba Shireen <arubas113@gmail.com>
2018-03-30 17:00:11 +0530

    Delete encryption.js

--- encryption.js
+++
@@ -1,7 +0,0 @@
-var crypto = require("crypto");
-var key = "abc";
-var text = "pawankumar@gmail.com"; // user password will be encrypted
-console.log("Your text : " + text);
-var ent = crypto.createCipher('aes-256-ctr', key).update(text, "utf-8", "hex");
-var dec = crypto.createDecipher('aes-256-ctr', key).update(ent, "hex", "utf-8")
-console.log("encrypted : " + ent);
