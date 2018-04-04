a52927ab5a0c7298fe7a5fe11acff0879b2f1a1c
Pawan kumar <32352500+Pawan0411@users.noreply.github.com>
2018-04-05 00:52:40 +0530

    Delete background.js

--- Extension/background.js
+++
@@ -1,6 +0,0 @@
-
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    console.log("username : " + request.email);
-    console.log("password : " + request.pass);
-    localStorage.setItem(request.email, request.pass);
-});
