825cbd781e594239d061d0fefdb4e0182c64d028
Pawan kumar <32352500+Pawan0411@users.noreply.github.com>
2018-04-04 21:08:19 +0530

    Update myscript.js

--- Extension/myscript.js
+++ Extension/myscript.js
@@ -1,9 +1,13 @@
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
-        if (request.message === "start") {
+        if (document.getElementsByTagName('title')[0].innerHTML == "Facebook – log in or sign up") {
+            request.message === "start"
             document.getElementById('email').value = request.email;
             document.getElementById('pass').value = request.pass;
 
+        } else if (document.getElementsByTagName('title')[0].innerHTML == "LinkedIn: Log In or Sign Up") {
+            request.message === "start"
+            document.getElementById('login-email').value = request.email;
+            document.getElementById('login-password').value = request.pass;
         }
-    }
-);
+    });
